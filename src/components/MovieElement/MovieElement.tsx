496e7d18a5aefc9f3a8e49a7b7910df69dd35095
Timo Huennebeck <timohuennebeck@Timos-Air.fritz.box>
2023-04-06 14:32:21 +0200

    add like button to movie element

--- src/components/MovieElement/MovieElement.tsx
+++ src/components/MovieElement/MovieElement.tsx
@@ -1,10 +1,32 @@
-// libraries
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./MovieElement.scss";
 import { Link } from "react-router-dom";
 import { MovieInterface } from "../../interfaces/appInterfaces";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 export default function MovieElement({ item }: MovieInterface) {
+  const [favorite, setFavorite] = useState(false);
+
+  // load favorite status from localStorage
+  useEffect(() => {
+    const storedFavorite = localStorage.getItem(`movie-${item.id}-favorite`);
+    if (storedFavorite) {
+      setFavorite(JSON.parse(storedFavorite));
+    }
+  }, [item.id]);
+
+  // function to handle favorite status change
+  const handleFavoriteChange = () => {
+    const newFavoriteStatus = !favorite;
+    setFavorite(newFavoriteStatus);
+
+    // save favorite status to localStorage
+    localStorage.setItem(
+      `movie-${item.id}-favorite`,
+      JSON.stringify(newFavoriteStatus)
+    );
+  };
+
   return (
     <Link to={`movies/${item.id}`} className="movie">
       <div className="movie__box">
@@ -13,6 +35,19 @@ export default function MovieElement({ item }: MovieInterface) {
           alt=""
           className="movie__box-img"
         />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            handleFavoriteChange();
+          }}
+          className="movie__like"
+        >
+          {favorite ? (
+            <AiFillHeart className="movie__like-filledIn" />
+          ) : (
+            <AiOutlineHeart className="movie__like-empty" />
+          )}
+        </button>
       </div>
 
       <div className="movie__content">
