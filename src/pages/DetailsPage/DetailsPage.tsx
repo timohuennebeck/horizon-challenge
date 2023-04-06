64d17c8059e5108174a9591b794ae9a3c03c48a4
Timo Huennebeck <timohuennebeck@Timos-Air.fritz.box>
2023-04-06 16:18:41 +0200

    add styling to details pages

--- src/pages/DetailsPage/DetailsPage.tsx
+++ src/pages/DetailsPage/DetailsPage.tsx
@@ -5,9 +5,11 @@ import { Link, useParams } from "react-router-dom";
 import "./DetailsPage.scss";
 import { DetailsInterface } from "../../interfaces/appInterfaces";
 import { MovieDetails } from "../../utils/apiCalls";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 export default function DetailsPage() {
   const [data, setData] = useState<DetailsInterface | null>(null);
+  const favorite = false;
 
   const { movieId } = useParams();
 
@@ -24,15 +26,27 @@ export default function DetailsPage() {
 
   return (
     <div className="details">
-      <img
-        className="details__img"
-        src={`http://image.tmdb.org/t/p/w500/${data.poster_path}`}
-        alt=""
-      />
+
+      <div className="details__box">
+        <img
+          className="details__box-img"
+          src={`http://image.tmdb.org/t/p/w500/${data.poster_path}`}
+          alt=""
+        />
+        <button className="details__box-like">
+          {favorite ? (
+            <AiFillHeart className="details__box-like-filledIn" />
+          ) : (
+            <AiOutlineHeart className="details__box-like-empty" />
+          )}
+        </button>
+      </div>
 
       <div className="details__content">
         <div className="details__content-box">
-          <h1 className="details__content-box-header">{data.original_title}</h1>
+          <h1 className="details__content-box-header">
+            {data.original_title}
+          </h1>
           <p className="details__content-box-overview">{data.overview}</p>
         </div>
 
--- src/pages/DetailsPageHome/DetailsPageHome.tsx
+++ src/pages/DetailsPageHome/DetailsPageHome.tsx
@@ -5,9 +5,11 @@ import { Link, useParams } from "react-router-dom";
 import "./DetailsPageHome.scss";
 import { DetailsInterface } from "../../interfaces/appInterfaces";
 import { MovieDetails } from "../../utils/apiCalls";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 export default function DetailsPageHome() {
   const [data, setData] = useState<DetailsInterface | null>(null);
+  const favorite = false;
 
   const { movieId } = useParams();
 
@@ -24,11 +26,21 @@ export default function DetailsPageHome() {
 
   return (
     <div className="details-home">
-      <img
-        className="details-home__img"
-        src={`http://image.tmdb.org/t/p/w500/${data.poster_path}`}
-        alt=""
-      />
+
+      <div className="details-home__box">
+        <img
+          className="details-home__box-img"
+          src={`http://image.tmdb.org/t/p/w500/${data.poster_path}`}
+          alt=""
+        />
+        <button className="details-home__box-like">
+          {favorite ? (
+            <AiFillHeart className="details-home__box-like-filledIn" />
+          ) : (
+            <AiOutlineHeart className="details-home__box-like-empty" />
+          )}
+        </button>
+      </div>
 
       <div className="details-home__content">
         <div className="details-home__content-box">
