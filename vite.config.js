92bdcf9f489da6aa52193e87a985d34a9eefef2a
timohuennebeck <hello@timohuennebeck.com>
2023-04-12 14:31:21 +0200

    fix bug of import.meta

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import reactPages from "vite-plugin-react-pages";
+import EnvironmentPlugin from "vite-plugin-environment";
 
 export default defineConfig({
-  plugins: [react(), reactPages()],
+  plugins: [react(), reactPages(), EnvironmentPlugin("all")],
   build: {
     outDir: "build",
   },
